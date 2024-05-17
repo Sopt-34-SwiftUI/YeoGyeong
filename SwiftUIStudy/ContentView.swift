4adc0ff5e67a585edf5ce809072a86da29a1fedb
0gonge <Challengef0802@gmail.com>
2024-05-17 22:53:53 +0900

    ImpCalculatorUI with Background Black

--- SwiftUIStudy/ContentView.swift
+++ SwiftUIStudy/ContentView.swift
@@ -9,137 +9,142 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            Spacer()
-            HStack{
+        ZStack{
+            Color.black.ignoresSafeArea()
+            VStack{
                 Spacer()
-                Text("50,2502")
-                    .padding()
-            }
-            HStack{
-                Text("7")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("8")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("9")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("X")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.orange)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-            }
-            
-            HStack{
-                Text("4")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("5")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("6")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("-")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.orange)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
+                HStack{
+                    Spacer()
+                    Text("50,2502")
+                        .padding()
+                        .foregroundColor(.white)
+                        .font(.system(size: 73))
+                }
+                HStack{
+                    Text("7")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("8")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("9")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("X")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.orange)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                }
+                
+                HStack{
+                    Text("4")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("5")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("6")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("-")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.orange)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    
+                }
+                HStack{
+                    Text("1")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("2")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("3")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("+")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.orange)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                }
+                
+                HStack{
+                    Text("0")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("0")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text(".")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.gray)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                    Text("=")
+                        .frame(width: 80,
+                               height: 80)
+                        .background(.orange)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .font(.system(size: 33))
+                }
                 
             }
-            HStack{
-                Text("1")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("2")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("3")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("+")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.orange)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-            }
-            
-            HStack{
-                Text("0")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("0")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text(".")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.gray)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-                Text("=")
-                    .frame(width: 80,
-                           height: 80)
-                    .background(.orange)
-                    .cornerRadius(40)
-                    .foregroundColor(.white)
-                    .font(.system(size: 33))
-            }
-            
         }
     }
 }
